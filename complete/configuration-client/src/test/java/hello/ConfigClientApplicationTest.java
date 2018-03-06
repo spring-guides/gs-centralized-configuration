0ad162ab473e65acbf2be91a774f97f1d5b5afb8
Greg Turnquist <gturnquist@pivotal.io>
2018-03-06 12:22:21 -0600

    Upgrade to Spring Boot 2.0.0.RELEASE

--- complete/configuration-client/src/test/java/hello/ConfigClientApplicationTest.java
+++ complete/configuration-client/src/test/java/hello/ConfigClientApplicationTest.java
@@ -16,13 +16,13 @@
 
 package hello;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.EnvironmentTestUtils;
+import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.cloud.context.refresh.ContextRefresher;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -43,7 +43,9 @@ public class ConfigClientApplicationTest {
     @Test
     public void contextLoads() {
         assertThat(controller.getMessage()).isNotEqualTo("Hello test");
-        EnvironmentTestUtils.addEnvironment(environment, "message:Hello test");
+        TestPropertyValues
+            .of("message:Hello test")
+            .applyTo(environment);
         assertThat(controller.getMessage()).isNotEqualTo("Hello test");
         refresher.refresh();
         assertThat(controller.getMessage()).isEqualTo("Hello test");
