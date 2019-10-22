9af0cff36004e8c4946231ff78a38757ba178146
Jay Bryant <jbryant@pivotal.io>
2019-10-22 11:57:09 -0500

    Upgrade to Spring Boot 2.2
    I upgraded to Spring Boot 2.2.0, which necessitated upgrading the test to work with JUnit 5 and replacing the Gradle wrappers. I also corrected an earlier mistake that I made in /initial/pom.xml.

---
+++ initial/configuration-client/src/main/java/com/example/centralizedconfigurationclient/CentralizedConfigurationClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.centralizedconfigurationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedConfigurationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedConfigurationClientApplication.class, args);
+	}
+
+}
---
+++ initial/configuration-client/src/test/java/com/example/centralizedconfigurationclient/CentralizedConfigurationClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centralizedconfigurationclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedConfigurationClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
